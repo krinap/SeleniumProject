fd8f257cacc19dd0498b541cc2f32bc8d8962f43
krinap <krinap311@gmail.com>
2019-11-17 22:51:13 -0500

    WallmartFile Added

---
+++ src/PracticeSelenium/WallmartWebsiteTesting.java
@@ -0,0 +1,11 @@
+package PracticeSelenium;
+
+public class WallmartWebsiteTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Wallmart");
+	}
+
+}
